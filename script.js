5e5f76a9fcb88756d8418bf6115c5f36919898ac
InnovationBham_1 <InnovationBham_1@UABiLab-1s-iMac.local>
2017-06-30 16:12:52 -0500

    Working on it

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+var empty = [ ]
+var symbol = ["+", "-", "/", "*"]
 $(document).ready(function(){
     $('span').mousedown(function(){
         $(this).css("background","orange")
@@ -6,52 +8,27 @@ $(document).ready(function(){
         $(".num").css("background","white")
         $(".symbol").css("background","darkgray")
     });
-    $('#num-0').mousedown(function(){
-        $('#readout').text("0")
+    $('.num').mousedown(function(){
 
-        
+       $('num-').append(empty).text();
+        console.log(empty)
+    
+   /* $('#add').mousedown(function(){
+        $('#readout').append("+");
     });
-    $('#num-1').mousedown(function(){
-        $('#readout').append("1")
-        
+     $('#subtract').mousedown(function(){
+        $('#readout').append("-");
     });
-    $('#num-2').mousedown(function(){
-        $('#readout').append("2")
-        
+     $('#multiply').mousedown(function(){
+        $('#readout').append("*");
     });
-    $('#num-3').mousedown(function(){
-        $('#readout').append("3")
+     $('#divide').mousedown(function(){
+        $('#readout').append("/");
+    });*/
+    $('#add').mousedown(function(){
         
-    });
-    $('#num-4').mousedown(function(){
-        $('#readout').append("4")
+        });
         
-    });
-    $('#num-5').mousedown(function(){
-        $('#readout').append("5")
-        
-    });
-    $('#num-6').mousedown(function(){
-        $('#readout').append("6")
-        
-    });
-    $('#num-7').mousedown(function(){
-        $('#readout').append("7")
-        
-    });
-    $('#num-8').mousedown(function(){
-        $('#readout').append("8")
-        
-    });
-    $('#num-9').mousedown(function(){
-        $('#readout').append("9")
-        
-    });
-    $('#clear').mousedown(function(){
-        $('#readout').empty();
-    });
-    $('#num-0').mousedown(function(){
-        $('#readout').append("0");
-        
+
     });
 });
