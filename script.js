890b12492c63012c323bf7209f748274f8bfc7c4
InnovationBham_1 <InnovationBham_1@UABiLab-1s-iMac.local>
2017-07-03 11:12:44 -0500

    final commit

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 var empty = [ ]
-var symbol = ["+", "-", "/", "*"]
+var symbol = ['+', '-', '/', '*', '.']
+
 $(document).ready(function(){
     $('span').mousedown(function(){
         $(this).css("background","orange")
@@ -8,27 +9,65 @@ $(document).ready(function(){
         $(".num").css("background","white")
         $(".symbol").css("background","darkgray")
     });
-    $('.num').mousedown(function(){
 
-       $('num-').append(empty).text();
+    $('#num-1').mousedown(function(){
+        $('#readout').append(1);
+     });
+     $('#num-2').mousedown(function(){
+         $('#readout').append(2);
+     });
+     $('#num-3').mousedown(function(){
+        $('#readout').append(3)
+     });
+     $('#num-4').mousedown(function(){
+         $('#readout').append(4); 
+     });
+     $('#num-5').mousedown(function(){
+         $('#readout').append(5);
+     });
+     $('#num-6').mousedown(function(){
+         $('#readout').append(6);
+     });
+     $('#num-7').mousedown(function(){
+         $('#readout').append(7);
+     });
+     $('#num-8').mousedown(function(){
+         $('#readout').append(8);
+     });
+     $('#num-9').mousedown(function(){
+         $('#readout').append(9);
+    });
+    $('#clear').mousedown(function(){
+        empty.lenth=0;
+        $('#readout').empty();
+    });
+     $('#num-0').mousedown(function(){
+        $('#readout').append(0);
+     });
+    $('#add').mousedown(function(){    
+        $('#readout').append(symbol[0]);
+    });
+    $('#subtract').mousedown(function(){
+        $('#readout').append(symbol[1]);
+    });
+    $('#multiply').mousedown(function(){ 
+        $('#readout').append(symbol[3]);
+    });
+    $('#divide').mousedown(function(){    
+        $('#readout').append(symbol[2]);
+    });
+    $('#decimal').mousedown(function(){    
+        $('#readout').append(symbol[4]);
+    });
+    $('#result').mousedown(function(){
+        empty.push(document.getElementById("readout").innerHTML) 
         console.log(empty)
-    
-   /* $('#add').mousedown(function(){
-        $('#readout').append("+");
-    });
-     $('#subtract').mousedown(function(){
-        $('#readout').append("-");
-    });
-     $('#multiply').mousedown(function(){
-        $('#readout').append("*");
-    });
-     $('#divide').mousedown(function(){
-        $('#readout').append("/");
-    });*/
-    $('#add').mousedown(function(){
-        
-        });
-        
-
+        empty.join(" ");
+        var solution = empty.join(" ");
+        var answer = eval(solution);
+        $('#readout').empty();
+        $('#readout').append(answer);
+        empty.length=0;
+        console.log(answer)
     });
 });
